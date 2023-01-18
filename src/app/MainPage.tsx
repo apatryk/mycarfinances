1d2a375c03cd56dab238b31a447391dff6b750aa
apatryk <patrykantecki@gmail.com>
2023-01-19 00:12:20 +0100

    fuel consumption calculation

--- src/app/MainPage.tsx
+++ src/app/MainPage.tsx
@@ -1,7 +1,39 @@
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+
+
 export const MainPage = () => {
-    return (
-      <>
-        asd
-      </>
-    );
-  };
+  const [fuel, setFuel] = useState(0);
+  const [distance, setDistance] = useState(0);
+  const onFuelChange = (e: any) => setFuel(e.target.value);
+  const onDistanceChange = (e: any) => setDistance(e.target.value);
+  const fuelConsumptionCalc = () => console.log(((fuel / distance)*100).toFixed(2));
+  return (
+    <>
+      <TextField
+        id="filled-number"
+        label="Zatankowane paliwo"
+        type="number"
+        value={fuel}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="filled"
+        onChange={onFuelChange}
+        
+      />
+      <TextField
+        id="filled-number"
+        label="Stan licznika"
+        type="number"
+        value={distance}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="filled"
+        onChange={onDistanceChange}
+      />
+      <button onClick={fuelConsumptionCalc}></button>
+    </>
+  );
+};
